7d29f400a5ff4c549de9ef62c48e74ae767d1a07
Liam Wiltshire <liam@w.iltshi.re>
2020-04-14 12:33:52 +0100

    For now, don't instrument console

--- src/Providers/ScoutApmServiceProvider.php
+++ src/Providers/ScoutApmServiceProvider.php
@@ -160,20 +160,16 @@ final class ScoutApmServiceProvider extends ServiceProvider
             __DIR__ . '/../../config/scout_apm.php' => config_path('scout_apm.php'),
         ]);
 
-        $instrumentQueries = false;
-
         $runningInConsole = $application->runningInConsole();
-        if (!$runningInConsole) {
-            $instrumentQueries = true;
+
+        if ($runningInConsole) {
+            return;
         }
+
+        $this->instrumentDatabaseQueries($agent, $connection);
 
         if ($agent->shouldInstrument(self::INSTRUMENT_LARAVEL_QUEUES)) {
-            $instrumentQueries = true;
             $this->instrumentQueues($agent, $application->make('events'), $runningInConsole);
-        }
-
-        if ($instrumentQueries) {
-            $this->instrumentDatabaseQueries($agent, $connection);
         }
 
         if ($runningInConsole) {
